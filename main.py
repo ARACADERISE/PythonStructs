4c2810b93203c35ac91285d2c46786b2171ebe17
GetStuffDone <36897041+ARACADERISE@users.noreply.github.com>
2020-09-20 00:47:31 -0500

    Update main.py

--- main.py
+++ main.py
@@ -6,7 +6,6 @@ casted_information = {}
 def _write_(information_1,information_2 = None):
 
     # "flushing" the file to be empty
-    os.remove('info.txt')
 
     with open('info.txt','w') as file:
 
@@ -51,12 +50,117 @@ class CreateStruct:
         self.StructItems = struct_items
         self.struct_names = []
         self.struct_name_values = []
+        self.updated_names = {}
+        self.update_values = {}
         self.information = {} # this will hold the struct names and information
         self.current_name_index = 0
         self.current_info_index = 0
 
         for i in struct_items:
             self.information.update({i:{}})
+        
+        for i in struct_items:
+            self.struct_names.append(i)
+    
+    def _init_items_(self, **kwargs) -> list:
+
+        LIST_OF_ITEMS = []
+
+        for val, item in kwargs.items():
+            if val == 'length':
+                if isinstance(item,bool):
+                    if item == True:
+                        LIST_OF_ITEMS.append(len(self.struct_names))
+            if val == "dict_":
+                if isinstance(item,bool):
+                    if item == True:
+                        LIST_OF_ITEMS.append(self.information)
+            if val == "names":
+                if isinstance(item,bool):
+                    if item == True:
+                        LIST_OF_ITEMS.append(self.struct_names)
+            if val == 'all':
+                if isinstance(item,bool):
+                    if item == True:
+                        LIST_OF_ITEMS.append(len(self.struct_names))
+                        LIST_OF_ITEMS.append(self.information)
+        
+        return LIST_OF_ITEMS
+    
+    def _grab_item_(self,item_type, from_):
+
+        for i in from_:
+            if isinstance(i,item_type):
+                return i
+    
+    def _grab_names_(self, not_null=False) -> list:
+        
+        if not_null == False:
+            return self.struct_names
+        else:
+            items = []
+            for i in self.struct_names:
+                if self.information[i] == {}:
+                    continue
+                else: items.append(i)
+            
+            if len(items) == 0:
+                items.append('All struct items are null')
+            return items
+    
+    @CastNewItem(['StructUpdateStructName','Updating struct name'])
+    def update_name(self, item_name, item_new_name) -> (dict,None):
+
+        index = 0
+        found = False
+        info = ''
+        for i in range(len(self.struct_names)):
+            if item_name == self.struct_names[i]:
+                index = i
+                found = True
+                break
+
+        if found == True:
+            self.struct_names[index] = item_new_name
+            if self.information[item_name] != {}:
+                info = self.information[item_name]
+            del(self.information[item_name])
+            self.information.update({self.struct_names[index]:info if info else {}})
+
+            if self.update_values:
+                if item_name in self.update_values:
+                    value = self.update_values[item_name]
+                    del(self.update_values[item_name])
+                    self.update_values.update({item_new_name:value})
+
+            self.updated_names.update({item_new_name:item_name})
+        
+            return self.updated_names
+    
+    @CastNewItem(['StructUpdateStructItemInfo','Updating struct item information/value'])
+    def update_value(self, item_name, new_value) -> (dict, None):
+
+        index = 0
+        found = False
+        for i in range(len(self.struct_names)):
+            if item_name == self.struct_names[i]:
+                index = 0
+                found = True
+                break
+        
+        if found == True:
+            if self.struct_name_values[index]:
+                self.struct_name_values[index] = new_value
+                self.update_values.update({item_name:new_value})
+                
+                return self.update_values
+            else: 
+                self.struct_name_values.append(new_value)
+                self.current_info_index += 1
+            self.information[item_name] = new_value
+
+
+            return None
 
     @CastNewItem(['NewStructItem','Adding new Struct item'])
     def add_items(self, struct_item_name, add_many = 0):
@@ -113,20 +217,26 @@ class CreateStruct:
             self.current_info_index += 1
     
     @CastNewItem(['PrintingStructItem','Printing the struct item'])
-    def print_struct_item(self, item_name):
+    def print_struct_item(self, item_name, extra = True):
 
+        item = ''
         if isinstance(item_name,list):
             for i in item_name:
                 if i in self.information:
                     if self.information[i] == {}:
                         self.information[i] = "NULL"
-                    print(i+':\t',self.information[i])
+                    if i in self.updated_names and extra == True:
+                        item = i + '(UPDATED)'
+                    else: item = i
+                    print(item+':\t',self.information[i])
         else:
             if item_name in self.information:
+                if item_name in self.updated_names and extra == True:
+                    item_name = item_name + '(UPDATED)'
                 print(item_name+':\t',self.information[item_name])
     
     @CastNewItem(['PrintAllItems','Printing all Struct items'])
-    def print_all(self):
+    def print_all(self, extra = True):
 
         tabs = 0
         tabs_ = ''
@@ -147,16 +257,17 @@ class CreateStruct:
                     tabs = len(all[i-1])-len(all[i])+1
                     tabs_ = ' '*tabs
 
+        item = ''
+        item_info = ''
         for i in all:
+            if i in self.updated_names and extra == True:item = i + '(UPDATED)'
+            else: item = i
+
+            if i in self.update_values and extra == True:item_info = self.update_values[i] + '(UPDATED)'
+            else: item_info = self.information[i]
+
             if i == item_to_tab:
-                print(i+f':\t{tabs_}',self.information[i])
-            else: print(i+':\t',self.information[i])
+                print(item+f':\t{tabs_}',item_info)
+            else: print(item+':\t',item_info)
 
             curr += 1
-
-STRUCT = CreateStruct(['ItemOne','ItemTwo','ItemThree'])
-STRUCT.add_items('No')
-STRUCT.AddInfo(['ItemOne','ItemTwo', 'ItemThree'],['info','info_2','info_3'])
-STRUCT.AddInfo('No','Boom. Structs in Python!')
-
-STRUCT.print_all()
